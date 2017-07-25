90afa9c463f9392d8dbf8984486a94bf063be933
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2017-07-25 09:36:16 +0900

    - Added a TOC to the Jupyter notebook - Updated the `find_nprocs_subgrids.py` script - Added a script to create date-formatted links for grads - Added info about rankfiles to map processes to cores by rank

--- GIS/find_nprocs_subgrids.py
+++ GIS/find_nprocs_subgrids.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from math import sqrt,floor
 
 def find_nprocs_subgrids(grid_setup,np_max):
@@ -12,8 +11,8 @@ def find_nprocs_subgrids(grid_setup,np_max):
     dygrid_nest = grid_setup['dygrid_nest']
     nested_grid_start_x = grid_setup['nested_grid_start_x']
     nested_grid_start_y = grid_setup['nested_grid_start_y']    
-    print('Possible solutions with number of processes (nprocs) and grid sizes (ip,jp)')
-    print('nprocs, ip x jp, proc_col x proc_row') #, npx, npy')        
+    print('Possible solutions with number of processes (nprocs) and grid sizes (ip,jp):')
+    print('') 
     for np in range(np_max,0,-1):
         b = nested_grid_y / nested_grid_x
         a_x = (nested_grid_x*(1-dxgrid_nest/dxgrid_orig) + orig_grid_x)        
@@ -34,25 +33,37 @@ def find_nprocs_subgrids(grid_setup,np_max):
                 b = b_o
             if b<2:
                 b=2                
-            dx_guess = sqrt(a2)
-            dx_b = floor(dx_guess/b)
-            dx_e = int(dx_guess*b)
-#             print(dx_b, dx_e,b)
-            for dx in range(dx_b,dx_e):
-                dy = a2/dx
-                nx = nested_grid_start_x/dx
-                ny = nested_grid_start_y/dy
-                rx = (orig_grid_x-(nested_grid_x*dxgrid_nest)/dxgrid_orig - nested_grid_start_x)/dx
-                ry = (orig_grid_y-(nested_grid_y*dygrid_nest)/dygrid_orig - nested_grid_start_y)/dy
-                if (dy==floor(dy)):
+            ip_guess = sqrt(a2)
+            ip_b = floor(ip_guess/b)
+            ip_e = int(ip_guess*b)
+            for ip in range(ip_b,ip_e):
+                jp = a2/ip
+                nprocs_x = nested_grid_start_x/ip
+                nprocs_y = nested_grid_start_y/jp
+                rprocs_x = (orig_grid_x-(nested_grid_x*dxgrid_nest)/dxgrid_orig - nested_grid_start_x)/ip
+                rprocs_y = (orig_grid_y-(nested_grid_y*dygrid_nest)/dygrid_orig - nested_grid_start_y)/jp
+                if (jp==floor(jp)):
 #                     print(np,' Valid dy:',dx,int(dy),';',nx,ny,rx,ry)
-                        p_col = (orig_grid_x + nested_grid_x*(1 - dxgrid_nest/dxgrid_orig) ) / dx
-                        p_row = (orig_grid_y + nested_grid_y*(1 - dygrid_nest/dygrid_orig) ) / dy
-                        if (nx==floor(nx)) and (ny==floor(ny)) and \
-                            (rx==floor(rx)) and (ry==floor(ry)) and \
+                        p_col = (orig_grid_x + nested_grid_x*(1 - dxgrid_nest/dxgrid_orig) ) / ip
+                        p_row = (orig_grid_y + nested_grid_y*(1 - dygrid_nest/dygrid_orig) ) / jp
+                        if (nprocs_x==floor(nprocs_x)) and (nprocs_y==floor(nprocs_y)) and \
+                            (rprocs_x==floor(rprocs_x)) and (rprocs_y==floor(rprocs_y)) and \
                             (p_row==floor(p_row)) and (p_col==floor(p_col)) :
-                            print(np,dx,'x',int(dy),int(p_col),'x',int(p_row))
-#                               int(nx),int(ny),rx,ry)
+                            n_procs_nested_grid = int(p_col-nprocs_x-rprocs_x)*int(p_row-nprocs_y-rprocs_y)
+                            n_procs_orig_grid = int(np - n_procs_nested_grid)
+                            print('Number of processes, ip x jp, proc_cols x proc_rows') #, npx, npy')   
+                            print(np,',',ip,'x',int(jp),',',int(p_col),'x',int(p_row))
+                            print('procs in orig grid; (before+after) x (below+above) nested grid')
+                            print(n_procs_orig_grid,';(',int(nprocs_x),'+',int(rprocs_x),')x(',int(nprocs_y),'+',int(rprocs_y),')')
+                            print('procs in nested grid:')
+                            print(n_procs_nested_grid,';',int(p_col-nprocs_x-rprocs_x),'x',int(p_row-nprocs_y-rprocs_y))
+                            o2n = sqrt(dxgrid_orig*dygrid_orig/dxgrid_nest/dygrid_nest)
+                            print('Number of cores needed:',int(n_procs_nested_grid+n_procs_orig_grid/o2n))
+                            print('')
+                            
+# So now we need some heuristic to group processes from the orig grid.
+# We assume that we can put sqrt(dxgrid_orig*dygrid_orig/dxgrid_nest/dygrid_nest) processes per cor
+# because they are slower
 
 
 grid_setup = {
