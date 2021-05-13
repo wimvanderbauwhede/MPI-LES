2c007b09889ded0f31bbe7c69533c90b0ef9b265
Wim Vanderbauwhede <Wim.Vanderbauwhede@mail.be>
2021-05-13 17:53:39 +0100

    nm

--- GeneratedCodeForMemoryReduction/ASTInstance.hs
+++ GeneratedCodeForMemoryReduction/ASTInstance.hs
@@ -1,3 +1,4 @@
+-- module_adam_feedbf_les_press_velfg_ve_etc_superkernel
 module ASTInstance ( ast
         , functionSignaturesList
         , stencilDefinitionsList
@@ -74,44 +75,44 @@ stencilDefinitionsList = []
 
 mainArgDeclsList = [
       ("bmask1_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["bmask1_0"] )
-    , ("g_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["g_0"] )
-    , ("dt_0" , MkFDecl "real" Nothing (Just In) ["dt_0"] )
-    , ("f_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["f_0"] )
-    , ("pav_0" , MkFDecl "real" Nothing (Just In) ["pav_0"] )
-    , ("nrd_0" , MkFDecl "integer" Nothing (Just In) ["nrd_0"] )
-    , ("dzn_0" , MkFDecl "real"  (Just [84]) (Just In) ["dzn_0"] )
-    , ("usum_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["usum_0"] )
-    , ("w_0" , MkFDecl "real"  (Just [7594998]) (Just In) ["w_0"] )
-    , ("p0_0" , MkFDecl "real"  (Just [7528338]) (Just In) ["p0_0"] )
-    , ("gold_0" , MkFDecl "real"  (Just [7200000]) (Just In) ["gold_0"] )
-    , ("dy1_0" , MkFDecl "real"  (Just [302]) (Just In) ["dy1_0"] )
-    , ("dxs_0" , MkFDecl "real"  (Just [301]) (Just In) ["dxs_0"] )
     , ("fold_0" , MkFDecl "real"  (Just [7200000]) (Just In) ["fold_0"] )
-    , ("v_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["v_0"] )
-    , ("h_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["h_0"] )
-    , ("vsum_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["vsum_0"] )
-    , ("ro_0" , MkFDecl "real" Nothing (Just In) ["ro_0"] )
-    , ("u_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["u_0"] )
-    , ("dzs_0" , MkFDecl "real"  (Just [84]) (Just In) ["dzs_0"] )
     , ("wsum_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["wsum_0"] )
-    , ("hold_0" , MkFDecl "real"  (Just [7200000]) (Just In) ["hold_0"] )
-    , ("delx1_0" , MkFDecl "real"  (Just [80]) (Just In) ["delx1_0"] )
-    , ("beta_0" , MkFDecl "real" Nothing (Just In) ["beta_0"] )
-    , ("cmask1_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["cmask1_0"] )
     , ("dmask1_0" , MkFDecl "real"  (Just [7478728]) (Just In) ["dmask1_0"] )
+    , ("dxs_0" , MkFDecl "real"  (Just [301]) (Just In) ["dxs_0"] )
+    , ("pav_0" , MkFDecl "real" Nothing (Just In) ["pav_0"] )
+    , ("w_0" , MkFDecl "real"  (Just [7594998]) (Just In) ["w_0"] )
+    , ("dzs_0" , MkFDecl "real"  (Just [84]) (Just In) ["dzs_0"] )
+    , ("dy1_0" , MkFDecl "real"  (Just [302]) (Just In) ["dy1_0"] )
+    , ("p0_0" , MkFDecl "real"  (Just [7528338]) (Just In) ["p0_0"] )
+    , ("f_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["f_0"] )
+    , ("dt_0" , MkFDecl "real" Nothing (Just In) ["dt_0"] )
+    , ("cmask1_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["cmask1_0"] )
     , ("dx1_0" , MkFDecl "real"  (Just [303]) (Just In) ["dx1_0"] )
+    , ("usum_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["usum_0"] )
+    , ("beta_0" , MkFDecl "real" Nothing (Just In) ["beta_0"] )
+    , ("v_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["v_0"] )
+    , ("g_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["g_0"] )
+    , ("hold_0" , MkFDecl "real"  (Just [7200000]) (Just In) ["hold_0"] )
+    , ("h_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["h_0"] )
+    , ("delx1_0" , MkFDecl "real"  (Just [80]) (Just In) ["delx1_0"] )
+    , ("alpha_0" , MkFDecl "real" Nothing (Just In) ["alpha_0"] )
+    , ("dzn_0" , MkFDecl "real"  (Just [84]) (Just In) ["dzn_0"] )
+    , ("gold_0" , MkFDecl "real"  (Just [7200000]) (Just In) ["gold_0"] )
     , ("dys_0" , MkFDecl "real"  (Just [301]) (Just In) ["dys_0"] )
     , ("rhsav_0" , MkFDecl "real" Nothing (Just In) ["rhsav_0"] )
-    , ("alpha_0" , MkFDecl "real" Nothing (Just In) ["alpha_0"] )
-    , ("fz_0" , MkFDecl "real"  (Just [7338681]) (Just Out) ["fz_0"] )
+    , ("nrd_0" , MkFDecl "integer" Nothing (Just In) ["nrd_0"] )
+    , ("u_0" , MkFDecl "real"  (Just [7503492]) (Just In) ["u_0"] )
+    , ("ro_0" , MkFDecl "real" Nothing (Just In) ["ro_0"] )
+    , ("vsum_0" , MkFDecl "real"  (Just [7338681]) (Just In) ["vsum_0"] )
     , ("f_4" , MkFDecl "real"  (Just [7338681]) (Just Out) ["f_4"] )
-    , ("p1_1" , MkFDecl "real"  (Just [7528338]) (Just Out) ["p1_1"] )
     , ("sm_0" , MkFDecl "real"  (Just [7528338]) (Just Out) ["sm_0"] )
-    , ("fy_0" , MkFDecl "real"  (Just [7338681]) (Just Out) ["fy_0"] )
-    , ("g_4" , MkFDecl "real"  (Just [7338681]) (Just Out) ["g_4"] )
-    , ("rhs_2" , MkFDecl "real"  (Just [7478728]) (Just Out) ["rhs_2"] )
-    , ("h_4" , MkFDecl "real"  (Just [7338681]) (Just Out) ["h_4"] )
+    , ("p1_1" , MkFDecl "real"  (Just [7528338]) (Just Out) ["p1_1"] )
     , ("fx_0" , MkFDecl "real"  (Just [7338681]) (Just Out) ["fx_0"] )
+    , ("g_4" , MkFDecl "real"  (Just [7338681]) (Just Out) ["g_4"] )
+    , ("fy_0" , MkFDecl "real"  (Just [7338681]) (Just Out) ["fy_0"] )
+    , ("h_4" , MkFDecl "real"  (Just [7338681]) (Just Out) ["h_4"] )
+    , ("fz_0" , MkFDecl "real"  (Just [7338681]) (Just Out) ["fz_0"] )
+    , ("rhs_2" , MkFDecl "real"  (Just [7478728]) (Just Out) ["rhs_2"] )
   ]
 
 scalarisedArgsList = [
@@ -157,3 +158,110 @@ origNamesList = [
     ,( "velnw_map_44",[("ro",[("ro_0",In)]), ("dys",[("dys_0",In)]), ("dt",[("dt_0",In)]), ("p0",[("p0_2",In)]), ("v",[("v_0",In),("v_1",Out)]), ("g",[("g_4",In)])])
     ,( "velnw_map_52",[("ro",[("ro_0",In)]), ("dzs",[("dzs_0",In)]), ("dt",[("dt_0",In)]), ("p0",[("p0_2",In)]), ("w",[("w_0",In),("w_1",Out)]), ("h",[("h_4",In)])])
   ]
+
+{-
+f_0 :: Vec 7338681 Float
+g_0 :: Vec 7338681 Float
+h_0 :: Vec 7338681 Float
+fold_0 :: Vec 7200000 Float
+gold_0 :: Vec 7200000 Float
+hold_0 :: Vec 7200000 Float
+usum_0 :: Vec 7338681 Float
+u_0 :: Vec 7503492 Float
+bmask1_0 :: Vec 7503492 Float
+vsum_0 :: Vec 7338681 Float
+v_0 :: Vec 7503492 Float
+cmask1_0 :: Vec 7503492 Float
+wsum_0 :: Vec 7338681 Float
+w_0 :: Vec 7594998 Float
+dmask1_0 :: Vec 7478728 Float
+alpha_0 :: Float
+dt_0 :: Float
+beta_0 :: Float
+dx1_0 :: SVec 303 Float
+dy1_0 :: SVec 302 Float
+dzs_0 :: SVec 84 Float
+dzn_0 :: SVec 84 Float
+delx1_0 :: SVec 80 Float
+dxs_0 :: SVec 301 Float
+dys_0 :: SVec 301 Float
+rhsav_0 :: Float
+p0_0 :: Vec 7528338 Float
+nrd_0 :: Int
+pav_0 :: Float
+ro_0 :: Float
+
+fx_0 :: Vec 7338681 Float
+fy_0 :: Vec 7338681 Float
+fz_0 :: Vec 7338681 Float
+sm_0 :: Vec 7528338 Float
+rhs_2 :: Vec 7478728 Float
+p1_1 :: Vec 7528338 Float
+f_4 :: Vec 7338681 Float
+g_4 :: Vec 7338681 Float
+h_4 :: Vec 7338681 Float
+
+velnw_map_52 :: (Float,Float,Float) -> (Float,Float,Float) -> Float
+velnw_map_44 :: (Float,Float,Float) -> (Float,Float,Float) -> Float
+velnw_map_36 :: (Float,Float,Float) -> (Float,Float,Float) -> Float
+velfg_map_95 :: (Float,Float,Float,Float) -> (Float,Float,Float) -> Float
+velfg_map_175 :: (Float,Float,Float) -> (Float,Float,Float) -> Float
+velfg_map_135 :: (Float,Float,Float,Float) -> (Float,Float,Float) -> Float
+press_reduce_78 :: (Float,Float,Float) -> (Float,Float) -> Float -> (Float,Float)
+press_reduce_129 :: (Float,Float,Float) -> (Float,Float) -> Float -> (Float,Float)
+press_map_97 :: (Float,Float,Float,Int) -> (Float,Float,Float) -> (Float,Float)
+press_map_87 :: Float -> Float -> Float
+press_map_65 :: (Float,Float,Float,Float) -> (Float,Float,Float,Float,Float,Float,Float) -> Float
+press_map_138 :: Float -> Float -> Float
+les_map_240 :: (Float,Float,Float,Float) -> (Float,Float,Float,Float,Float) -> Float
+les_map_202 :: (Float,Float,Float,Float,Float) -> (Float,Float,Float,Float,Float) -> Float
+les_map_164 :: (Float,Float,Float,Float,Float) -> (Float,Float,Float,Float,Float) -> Float
+les_map_119 :: (Float,Float,Float,Float,Float) -> (Float,Float,Float) -> Float
+feedbf_map_67 :: (Float,Float,Float,Float,Float,Float) -> (Float,Float,Float)
+feedbf_map_49 :: (Float,Float,Float) -> (Float,Float,Float,Float,Float,Float,Float,Float,Float) -> (Float,Float,Float,Float,Float,Float)
+adam_map_36 :: (Float,Float,Float,Float,Float,Float) -> (Float,Float,Float,Float,Float,Float)
+
+main :: (Vec 7338681 Float,Vec 7338681 Float,Vec 7338681 Float,Vec 7200000 Float,Vec 7200000 Float,Vec 7200000 Float,Vec 7338681 Float,Vec 7503492 Float,Vec 7503492 Float,Vec 7338681 Float,Vec 7503492 Float,Vec 7503492 Float,Vec 7338681 Float,Vec 7594998 Float,Vec 7478728 Float,Float,Float,Float,SVec 303 Float,SVec 302 Float,SVec 84 Float,SVec 84 Float,SVec 80 Float,SVec 301 Float,SVec 301 Float,Float,Vec 7528338 Float,Int,Float,Float) -> (Vec 7338681 Float,Vec 7338681 Float,Vec 7338681 Float,Vec 7528338 Float,Vec 7478728 Float,Vec 7528338 Float,Vec 7338681 Float,Vec 7338681 Float,Vec 7338681 Float)
+main (f_0,g_0,h_0,fold_0,gold_0,hold_0,usum_0,u_0,bmask1_0,vsum_0,v_0,cmask1_0,wsum_0,w_0,dmask1_0,alpha_0,dt_0,beta_0,dx1_0,dy1_0,dzs_0,dzn_0,delx1_0,dxs_0,dys_0,rhsav_0,p0_0,nrd_0,pav_0,ro_0) =
+  let
+     -- adam_map_36
+    (f_1,fold_1,g_1,gold_1,h_1,hold_1) = unzipt $ map adam_map_36 (zipt (f_0,g_0,h_0,fold_0,gold_0,hold_0))
+     -- feedbf_map_49
+    (fx_0,fy_0,fz_0,usum_1,vsum_1,wsum_1) = unzipt $ map (feedbf_map_49 (alpha_0,dt_0,beta_0)) (zipt (usum_0,u_0,bmask1_0,vsum_0,v_0,cmask1_0,wsum_0,w_0,dmask1_0))
+     -- feedbf_map_67
+    (f_2,g_2,h_2) = unzipt $ map feedbf_map_67 (zipt (f_1,fx_0,g_1,fy_0,h_1,fz_0))
+     -- les_map_119
+    sm_0 =  map (les_map_119 (dx1_0,dy1_0,dzs_0,dzn_0,delx1_0)) (zipt (u_0,v_0,w_0))
+     -- les_map_164
+    f_3 =  map (les_map_164 (dy1_0,dx1_0,dzn_0,dzs_0,dxs_0)) (zipt (sm_0,u_0,v_0,w_0,f_2))
+     -- les_map_202
+    g_3 =  map (les_map_202 (dy1_0,dx1_0,dzn_0,dzs_0,dys_0)) (zipt (sm_0,u_0,v_0,w_0,g_2))
+     -- les_map_240
+    h_3 =  map (les_map_240 (dzn_0,dx1_0,dy1_0,dzs_0)) (zipt (sm_0,u_0,w_0,v_0,h_2))
+     -- press_map_65
+    rhs_1 =  map (press_map_65 (dx1_0,dy1_0,dzn_0,dt_0)) (zipt (u_0,v_0,w_0,f_3,g_3,h_3,rhs_0))
+     -- press_reduce_78
+    (area_1,rhsav_1) =  fold (press_reduce_78 (dx1_0,dy1_0,dzn_0)) (rhsav_0,area_0) rhs_1
+     -- press_map_87
+    rhs_2 =  map (press_map_87 rhsav_1) rhs_1
+     -- press_map_97
+    (p0_1,p1_1) = unzipt $ map (press_map_97 (dzs_0,dys_0,dxs_0,nrd_0)) (zipt (p0_0,rhs_2,p1_0))
+     -- press_reduce_129
+    (pav_1,pco_1) =  fold (press_reduce_129 (dx1_0,dy1_0,dzn_0)) (pav_0,pco_0) p0_1
+     -- press_map_138
+    p0_2 =  map (press_map_138 pav_1) p0_1
+     -- velfg_map_95
+    f_4 =  map (velfg_map_95 (dx1_0,dy1_0,dzs_0,dzn_0)) (zipt (u_0,v_0,w_0))
+     -- velfg_map_135
+    g_4 =  map (velfg_map_135 (dy1_0,dx1_0,dzs_0,dzn_0)) (zipt (u_0,v_0,w_0))
+     -- velfg_map_175
+    h_4 =  map (velfg_map_175 (dzn_0,dx1_0,dy1_0)) (zipt (u_0,w_0,v_0))
+     -- velnw_map_36
+    u_1 =  map (velnw_map_36 (ro_0,dxs_0,dt_0)) (zipt (p0_2,u_0,f_4))
+     -- velnw_map_44
+    v_1 =  map (velnw_map_44 (ro_0,dys_0,dt_0)) (zipt (p0_2,v_0,g_4))
+     -- velnw_map_52
+    w_1 =  map (velnw_map_52 (ro_0,dzs_0,dt_0)) (zipt (p0_2,w_0,h_4))
+  in
+    (fx_0,fy_0,fz_0,sm_0,rhs_2,p1_1,f_4,g_4,h_4)
+-}
